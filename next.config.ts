08b671b89771d656a92934747adfd123b565feec
Maria Olívia <168108003+marowyck@users.noreply.github.com>
2024-11-30 19:18:41 -0300

    header

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  webpack(config: { module: { rules: { test: RegExp; issuer: { and: RegExp[]; }; use: string[]; }[]; }; }) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: { and: [/\.(js|ts)x?$/] },
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
+
+module.exports = nextConfig;
+
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import Header from '../components/Header/header'
-/*import Technologies from '../components/Technologies/technologies'
-import Articles from '../components/Articles/articles'
+/*import Articles from '../components/Articles/articles'
 import Projects from '../components/Projects/projects'
 import Contact from '../components/Contact/contact'*/
+import "boxicons/css/boxicons.min.css";
+
 
 export default function Home() {
   return (
